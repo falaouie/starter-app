8c2c1fd38547e47b1cf8b56ac648e388b5022623
Fadi Alaouie <falaouie@yahoo.com>
2022-07-08 15:42:12 +0300

    showing & persisting items

--- renderer/app.js
+++ renderer/app.js
@@ -1,5 +1,5 @@
 const { ipcRenderer } = require("electron")
-
+const items = require('./items')
 
 
 // Dom nodes
@@ -53,7 +53,9 @@ let showModal = document.getElementById('show-modal'),
 
     // Listen for new item from main process
     ipcRenderer.on('new-item-success', (e, newItem) => {
-        console.log(newItem);
+        
+        // Add new item to "items" node
+        items.addItem(newItem, true)
 
         // Enable buttons
         toggleModalButtons()
